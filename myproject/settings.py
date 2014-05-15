6907553f5c103266c4a05d4592583e57fed7d738
Truong Dao <nientruong.bb@gmail.com>
2014-05-15 23:18:20 +0700

    change database connection to mysql type

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,7 +58,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'myproject',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '',
+
     }
 }
 
